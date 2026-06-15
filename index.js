67b6d7547de0448d4b606a50730d97b2fffb9343
michia <michiagayepadilla@gmail.com>
2026-06-15 16:20:45 -0400

    "adding middleware"

--- index.js
+++ index.js
@@ -2,13 +2,20 @@
 
 import express from "express";
 
+// Part Two: Middleware (installed)
+import morgan from "morgan";
+
 const app = express();
 const port = 3000;
 
 app.set("view engine", "ejs");
+
+app.use(morgan("dev"));
+
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
+
 
 app.post("/submit", (req, res) => {
 console.log(req.body);
@@ -19,3 +26,16 @@ res.send("Success!")
 app.get("/", (req, res) => {
   res.render("home", { title: "App", message: "Welcome!" });
 });
+
+app.get('/about', (req, res) => {
+    res.render('about', {title: 'App', message: 'Welcome!'})
+})
+
+app.get("/:name", (req,res) => {
+    res.render("about")
+})
+
+app.listen(port, () =< {
+    console.log (`Server is Running on port ${port}`);
+})
+
