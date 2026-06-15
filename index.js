f901e109d7dc58691f51f13089012d0e68496431
michia <michiagayepadilla@gmail.com>
2026-06-15 15:31:34 -0400

    "adding .post"

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-// Part One: Routes, Templates, and Views 
-
+// Part One: Routes, Templates, and Views
 
 import express from "express";
 
 const app = express();
 const port = 3000;
 
-
 app.set("view engine", "ejs");
 app.listen(3000, () => {
-    console.log('Listening on port 3000')
-})
+  console.log("Listening on port 3000");
+});
 
+app.post("/submit", (req, res) => {
+console.log(req.body);
+res.send("Success!")
+});
+
+
+app.get("/", (req, res) => {
+  res.render("home", { title: "App", message: "Welcome!" });
+});
